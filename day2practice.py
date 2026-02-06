8acd2315107f304799324ee5ff54633c89d8f017
shubhankar <shubhankarb8@gmail.com>
2026-02-06 16:17:53 +0530

    day 2 practice and learning changes

--- day2practice.py
+++ day2practice.py
@@ -1,1 +1,28 @@
-str(10)
+print(str(10))
+print(float(20))
+print(int(40))
+
+
+#types built in functions (type)
+x = 10
+print(type(x))
+
+invoice_value = 3000.76
+print(type(invoice_value))
+
+invoice_name = "Invoice 001"
+print(type(invoice_name))
+
+#(len) built in function
+invoice_name = "Invoice 001"
+print(len(invoice_name))
+
+vendor_name = "ABC Corporation"
+print(len(vendor_name))
+
+pages = [1,2,3,4,5]
+print(len(pages))
+
+pages_types = {1,2,3,4,5}
+print(len(pages_types)) 
+
