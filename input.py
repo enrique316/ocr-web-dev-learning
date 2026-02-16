1702b275d4622d0f5e16947a43ba7b5dfc429207
shubhankar <shubhankarb8@gmail.com>
2026-02-16 19:41:49 +0530

    update

---
+++ input.py
@@ -0,0 +1,10 @@
+"""whatever users types in the input it is always stored as string"""
+
+username = input("enter your username: ")
+print(type(username))
+
+age =input("enter your age: ")
+converted_value = int(age)
+print(type(converted_value))
+
+
