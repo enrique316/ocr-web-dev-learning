a375e39dc1b867f40dab4812b691430e84368a60
shubhankar <shubhankarb8@gmail.com>
2026-02-19 16:36:31 +0530

    "push all "

---
+++ Day3/Floor Division assign operator.py
@@ -0,0 +1,22 @@
+
+# //= floor division assign operator # 
+extracted_value = 7
+extracted_value //= 2
+print(extracted_value) 
+
+a = 20
+a //= 7
+print(a)
+
+# with float #
+x = 7.1 
+x //= 2
+print(x)
+
+total_pages = 25
+total_bundels = 3 
+total_pages //= total_bundels
+print(total_pages)
+
+# floor division assign operator can be work with int and float # 
+ 
