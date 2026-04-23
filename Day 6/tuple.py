4c24ef4611be50f673c72c60d91c12384a89cf85
shubhankar <shubhankarb8@gmail.com>
2026-04-23 14:55:24 +0530

    " Day 6 started (tuple)"

---
+++ Day 6/tuple.py
@@ -0,0 +1,24 @@
+# Tuple in python #  sing ()
+"""
+2. Key idea (very important)
+
+👉 Tuple = like list
+👉 BUT with one big difference:
+
+👉 ❌ Cannot change values
+👉 ✅ Fixed once created
+"""
+a = (1,2,3)
+print(a)
+
+# tuple vs list comparison # 
+#with list you can change the value or value is mutable# 
+x= [1,2,3]
+x[0]=20
+print(x)
+👉#with tuple you can't change the value or the value with unmutable #  
+"""a = (1,2,3)
+a[0]=20"""
+👉# print(a) # this will throw error #  
+
+#
