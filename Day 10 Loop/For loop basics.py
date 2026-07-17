385c55bb3683e41f1763308ea1c9acb123f3d5d0
shubhankar <shubhankarb8@gmail.com>
2026-07-17 16:44:51 +0530

    Day 10 restarted

---
+++ Day 10 Loop/For loop basics.py
@@ -0,0 +1,17 @@
+# What is for loop #
+"""👉 A for loop is used to repeat the same task again and again for every item in a collection.
+
+👉 Instead of writing the same code multiple times, Python automatically repeats it for you."""
+
+"""names = { "diti", "gauri", "jaggu", "dhunni"}
+for names in names:
+    print(names) """
+
+#ocr examples #
+captured_data = [
+    "name",
+    "age",
+    "class" 
+]
+for captured_data in captured_data:
+    print(captured_data)
--- Day 10 Loop/While Loop Basics.py
+++ Day 10 Loop/While Loop Basics.py
@@ -1,51 +1,18 @@
+#What is while loop? 
 """
 1. What is a While Loop?
 
-👉 A while loop repeats a block of code as long as a condition remains True.
+👉 A while loop repeats a block of code as long as a condition is True.
 
-Unlike a for loop, which processes items from a sequence, a while loop keeps running until a condition becomes False.
-
-Real Meaning
-
-Suppose you want to print:
-
-0
-1
-2
-
-You could write:
-
-print(0)
-print(1)
-print(2)
-
-But that becomes inefficient for large numbers.
-
-A while loop automates the repetition.
-
-First Example
-i = 0
-
-while i < 3:
-    print(i)
-    i += 1
-Output
-0
-1
-2
+👉 When the condition becomes False, the loop automatically stops.
 """
+i = 0
+while i < 5:
+    print(i)
+    i+= 1
+#another example #
 
-"""name = 1
-while name < 4:
-    print(name)
-    name += 1"""
-
-"""data = 2
-while data < 5:
-    print(data)
-    data += 1"""
-
-name = 0
-while name <10:
-    print(name)
-    name += 1
+a = 1
+while a <7:
+    print(a)
+    a +=1
