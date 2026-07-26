375190b87799a10f5fcdb48e2ba7819cc936a64e
shubhankar <shubhankarb8@gmail.com>
2026-07-26 18:35:04 +0530

    day 10

---
+++ Day 10 Loop/enumerate.py
@@ -0,0 +1,24 @@
+# What is enumerate #
+"""
+names = [ "diti", "gauri", "Dhunni"]
+for index, fruit in enumerate(names):
+    print(index,":", fruit)
+#example #
+extracted_data = [
+    "Invoice No",
+    "Date",
+    "time",
+    "City",
+]
+for index, extracted_data in enumerate(extracted_data):
+    print(index,"-", extracted_data) """ 
+
+
+captured_data =[ 
+    "Name",
+    "amount",
+    "-", 
+
+]
+for n, captured_data in enumerate(captured_data):
+    print(n, ":", captured_data)
