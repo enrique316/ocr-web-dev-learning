9024aa36941e6a09ab7603f8031d075ef24a8c0e
shubhankar <shubhankarb8@gmail.com>
2026-08-04 16:50:54 +0530

    Day 11 Continues

--- Day 11 Funcations/funcation basics.py
+++ Day 11 Funcations/funcation basics.py
@@ -1,4 +1,23 @@
 # What is function # 
+""" What is a Function?
+
+A function is a reusable block of code that performs a specific task.
+
+Instead of writing the same code repeatedly, you write it once and call it whenever needed."""
 def hi():
     print("Welcome")
 hi()
+
+#____#
+
+def calculate():
+    a= 7
+    b= 5
+    print(a+b)
+calculate()
+
+#----#
+def duplicate_the_value():
+    for x in range(10):
+        print(x)
+duplicate_the_value()
---
+++ Day 11 Funcations/function with parameters.py
@@ -0,0 +1,15 @@
+# What is function with parameters #
+def greet(name):
+    print("Hi", name)
+
+greet("Shubhankar")
+greet("Gauri")
+
+# Another example #
+
+
+def eliminate(numbers):
+    print("removed", numbers)
+
+eliminate(1)
+eliminate(2)
