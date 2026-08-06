7986f318b99089d582382596a4d85a2f3c10339e
shubhankar <shubhankarb8@gmail.com>
2026-08-06 16:51:03 +0530

    Day 11 Contimues

---
+++ Day 11 Funcations/funcation with return.py
@@ -0,0 +1,23 @@
+# What is function with return # 
+""" The return keyword is used to send a value from a function back to the place where the function was called.
+
+Think of it like this:
+
+Parameter → Data goes into the function.
+Return → Data comes out of the function."""
+"""
+def add(a,b):
+    return a+b
+result = add(2,3)
+print(result) """ 
+
+# Another example # 
+def minus(a,b):
+    return a-b
+result = minus(5,7)
+print(result)
+
+def multiply( x, y,z,d):
+    return x*y*z*d
+get_data = multiply(2,2,4,16)
+print(get_data)
