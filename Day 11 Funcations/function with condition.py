34f4cc463601a47d6aabb62c529becb6fff36b56
shubhankar <shubhankarb8@gmail.com>
2026-09-17 07:04:31 +0530

    HTML Day 5 continues

---
+++ Day 11 Funcations/function with condition.py
@@ -0,0 +1,14 @@
+# what is function with condition in python 3 #
+""" def number(x):
+    if x >10:
+        print("valid")
+number(15) """ 
+
+# ---3
+
+def invoice(name):
+    if name == "INV100":
+        print("invoice found")
+
+
+invoice("INV100")
